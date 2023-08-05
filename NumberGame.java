d21a4ab64d1cb1b153d1c036a71855a3696fbdc9
PRASHANT DWIVEDI <pdwivedi04@gmail.com>
2023-08-05 12:48:59 +0530

    Score problem updated for first round but for second round score will not update and loop will continue irrespect of correct condition means if guess is correct in second attempt though loop will continue third problem is after second round it will not ask user choice it will exit automatically

--- NumberGame.java
+++ NumberGame.java
@@ -8,9 +8,10 @@ class CompareUserGuess
 	static int randomNum;
 	static int userInputNum;
 	static int i = 0;
+	int j=5;
 	
 	//initializing getRandomNum() method to get the random integer value.
-	static int getRandomNum()
+	int getRandomNum()
 		{
 		//initializing two variables for given range.
 		int maxNum = 3;
@@ -23,7 +24,7 @@ class CompareUserGuess
 		}
 		
 	//initializing getUserInputNum() method to get the integer value input from the user.
-	static int getUserInputNum()
+	int getUserInputNum()
 		{
 		//creating scanner class object.
 		Scanner s = new Scanner(System.in);
@@ -50,34 +51,62 @@ class CompareUserGuess
 		{
 			if (userInputNum == randomNum){
 			
-				System.out.println("Guess is correct" + randomNum);
+				System.out.println("Guess is correct " + "\nYour score is : "+ (j)+"/5");
 				i=5;
+
 				}
 			
 			else if ((randomNum>userInputNum)){
 			
-				System.out.println("Guess is too low" + randomNum);
+				System.out.println("Guess is too low ");
 				}
 			
 			
 			else{
 				
-				 System.out.println("Guess is too high"+ randomNum);
+				 System.out.println("Guess is too high ");
 				 }
 		}
 
 	//initializing fiveAttempts() method to run the entire process either upto equal nums or upto 4 more times.
 	void fiveAttempts (int userInputNum, int randomNum)
 		{
-		while (i<4)
+		
+		while (i<4 /*&& userInputNum != randomNum*/)
 			{
+				j--;
 				compare(getUserInputNum(),getRandomNum());
 				i++;
+				
 				//if (userInputNum == randomNum)
 				//break;
 			}
+		
+		/*System.out.println("For play again please press 1 \n For exit please press 2");
+		Scanner s = new Scanner(System.in);
+		int choice = s.nextInt();
+		switch(choice)
+			{
+				case 1:
+					playAgain();
+						
+				case 2:
+					System.out.println("EXIT");
+					break;
+
+			}
+			*/
+			
 					
 		}
+
+	void playAgain()
+		{
+			compare(getUserInputNum(),getRandomNum());
+			//if (userInputNum != randomNum)
+				//fiveAttempts (userInputNum, randomNum);
+		}
+		
 
 }
 
@@ -96,6 +125,31 @@ class NumberGame
 		if (ob.userInputNum != ob.randomNum)
 			ob.fiveAttempts (ob.userInputNum, ob.randomNum);
 
-        System.out.println("If you want to play again please press 1");
+        //System.out.println("Your score is 0/5");
+		//System.out.println("If you want to play again please press 1");
+
+		System.out.println("For play again please press 1 \n For exit please press 2");
+		Scanner s = new Scanner(System.in);
+		int choice = s.nextInt();
+		switch(choice)
+			{
+				case 1:
+					int i=0;
+					int j=5;
+					while(i<5)
+						{
+							ob.playAgain();
+							i++;
+							j--;
+							
+							
+						}
+						
+				case 2:
+					System.out.println("EXIT");
+					break;
+
+			}
+			
 	}
 }
